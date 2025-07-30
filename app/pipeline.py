5c7497ac49a4adbf787a69df8f2ca3a031e8ed8a
Soham Bhattacharyya <boomsoham@gmail.com>
2025-07-30 12:06:14 +0530

    removing mlflow for faster deployment to openshift

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,6 +1,5 @@
 from transformers import pipeline
 from app.config import ASR_MODEL, TRANSLATION_MODEL
-import mlflow
 
 # Load pipelines
 asr_pipeline = pipeline("automatic-speech-recognition", model=ASR_MODEL)
@@ -10,18 +9,13 @@ def transcribe_speech(filepath, target_language):
     if not filepath:
         return "No audio found, please retry.", []
 
-    with mlflow.start_run(nested=True):
-        mlflow.log_param("target_language", target_language)
-        mlflow.log_param("audio_file", filepath)
+    # Transcription
+    output = asr_pipeline(filepath)
+    transcription = output["text"]
 
-        # Transcription
-        output = asr_pipeline(filepath)
-        transcription = output["text"]
-        mlflow.log_text(transcription, "transcription.txt")
-
-        # Translation
-        text_translated = translator(transcription, src_lang="eng_Latn", tgt_lang=target_language)
-        translation = text_translated[0]['translation_text']
-        mlflow.log_text(translation, "translation.txt")
+    # Translation
+    text_translated = translator(transcription, src_lang="eng_Latn", tgt_lang=target_language)
+    translation = text_translated[0]['translation_text']
 
     return transcription, translation
+
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import gradio as gr
 from app.interface import get_mic_interface, get_file_interface
 
-import mlflow
-
 if __name__ == "__main__":
-    mlflow.set_experiment("Speech_Translation_App")
-
     demo = gr.TabbedInterface(
         [get_mic_interface(), get_file_interface()],
         ["Transcribe Microphone", "Transcribe Audio File"]
